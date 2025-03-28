31da3c4755707584944578072df286f6c737cca2
Sheldon-sre <xie_yang_susu@outlook.com>
2025-03-28 12:10:02 +1000

    Create values.go

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("123" + "456")
+	fmt.Println(5 / 3)
+	fmt.Println(5.0 / 3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!false)
+}
+
+//to demo the values in go
+//strings , integers, floats, boolean
