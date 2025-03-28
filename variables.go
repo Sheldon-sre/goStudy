3633ab8af78ccdd992dcd816d99cad9149340e94
Sheldon-sre <xie_yang_susu@outlook.com>
2025-03-28 21:00:52 +1000

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "hello world"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	e := "test"
+	fmt.Println(e)
+}
+
